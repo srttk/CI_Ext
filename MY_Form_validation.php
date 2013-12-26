36f38b731b167eb362a95c8e5839c9a99fee40b9
Sarath <sarath.hacks@gmail.com>
2013-12-26 17:04:04 +0530

    Update MY_Form_validation.php

--- MY_Form_validation.php
+++ MY_Form_validation.php
@@ -13,4 +13,13 @@ class MY_Form_validation extends CI_Form_validation {
      public function is_money($input) {
          return (bool) preg_match('/^[0-9]+(\.[0-9]{0,2})?$/', $input);
      }
+     public function is_unique($str, $field) {
+        $field_ar = explode('.', $field);
+        $query = $this->CI->db->get_where($field_ar[0], array($field_ar[1] => $str), 1, 0);
+        if ($query->num_rows() === 0) {
+            return TRUE;
+        }
+
+        return FALSE;
+    }
  }
